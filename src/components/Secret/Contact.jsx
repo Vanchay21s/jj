a9eab4ba654ebb78c4852bc86e33ca5e49666507
Vanchay21 <okpro21333@gmail.com>
2026-02-10 12:53:44 +0700

    Contact

--- src/components/Secret/Contact.jsx
+++ src/components/Secret/Contact.jsx
@@ -2,7 +2,7 @@ import { useRef } from "react";
 import emailjs from "@emailjs/browser";
 import { MdEmail } from "react-icons/md";
 import { IoMdPin } from "react-icons/io";
-import { FaPhoneSquareAlt } from "react-icons/fa";
+import { FaFacebook, FaGithub, FaLinkedin, FaPhoneSquareAlt, FaTelegram } from "react-icons/fa";
 
 const Contact = () => {
   const form = useRef();
@@ -12,10 +12,10 @@ const Contact = () => {
 
     emailjs
       .sendForm(
-        "service_7sfajgl",
-        "template_ixdn1kn",
+        "service_bkgcsd8",
+        "template_mvyb4lu",
         form.current,
-        "MfXiOqXLZm3SCHBoy"
+        "7csjX-OvJvgUzwv97",
       )
       .then(
         () => {
@@ -25,68 +25,111 @@ const Contact = () => {
         (error) => {
           console.log(error.text);
           alert("Failed to send message");
-        }
+        },
       );
   };
 
   return (
-    <article className=" bg-slate-100 dark:bg-gray-900 transition-colors duration-400 flex flex-col items-center">
-       <div className="md:flex justify-between w-full md:max-w-7xl p-4 transition-colors duration-400 ">
-           <div className="w-full md:w-1/2 pb-5 space-y-4 text-gray-700 dark:text-white transition-colors duration-400 ">
-               <h1 className="text-4xl font-bold ">Contact Information</h1>
-               <p>We are committed to processing the information in order to contact you and talk about your project.</p>
-               <div className="mt-10 flex items-center justify-start"> 
-                   <MdEmail size={25} className="mr-2"/>
-                   <p>chaydouble0@gmail.com</p>
-               </div>
-               <div className="flex items-center justify-start"> 
-                   <IoMdPin size={25} className="mr-2"/>
-                   <p>334 Kampong Rou, Svay Rieng</p>
-               </div>
-               <div className="flex items-center justify-start"> 
-                   <FaPhoneSquareAlt size={25} className="mr-2"/>
-                   <p>(855) 60446580</p>
-               </div>
-           </div>
-           <div className="w-full md:w-1/2 md:flex justify-end">
-               <form ref={form} onSubmit={sendEmail} className="w-full space-y-4 md:max-w-sm p-4 border border-gray-900 dark:border-gray-100 rounded bg-white dark:bg-gray-800 transition-colors duration-400 ">                       
-                   <input type="hidden" name="time"
-                       value={new Date().toLocaleString()}
-                   />
-                   <input type="hidden" name="title"
-                       value="Website Contact"
-                   />
-                   <input type="text" name="name" placeholder="Name"
-                       className="w-full p-3 border rounded border-gray-900 dark:border-gray-100 text-gray-700 dark:text-white transition-colors duration-400"
-                       required
-                   />
-                   <input type="email" name="user_email" placeholder="Email"
-                       className="w-full p-3 border rounded border-gray-900 dark:border-gray-100 text-gray-700 dark:text-white transition-colors duration-400"
-                       required
-                   />
-                   <textarea name="message"  placeholder="Message"
-                       className="w-full p-3 border rounded border-gray-900 dark:border-gray-100 text-gray-700 dark:text-white transition-colors duration-400"
-                       rows="5"
-                       required
-                   ></textarea>
-                   <button
-                       type="submit"
-                       className="w-full bg-purple-600 text-white px-6 py-2 rounded cursor-pointer hover:bg-purple-700 transition-colors duration-400"
-                   >
-                       Send Message
-                   </button>
-               </form>
-           </div>
-          
-       </div>
-       <footer className="w-full bg-white dark:bg-gray-900 shadow z-50 text-gray-700 dark:text-white transition-colors duration-400">
-           <div className="max-w-7xl mx-auto px-6 h-16 flex items-center justify-center">
-           <p className="font-medium">© 2026 Meas Vanchay. All Rights Reserved.</p>
-           </div>
-       </footer>
-   </article>
-
+    <article className=" bg-slate-100 dark:bg-gray-900 transition-colors duration-400 flex flex-col items-center py-6 ">
+      <div className="md:flex justify-between w-full md:max-w-7xl px-4 pt-16 transition-colors duration-400 ">
+        <div className="w-full md:w-1/2 pb-5 space-y-4 text-gray-700 dark:text-white transition-colors duration-400 ">
+          <h1 className="mb-6 text-2xl sm:text-6xl font-bold text-gray-700 dark:text-gray-300">Contact Information</h1>
+          <p>
+            I’m currently free and open to new opportunities, so please feel
+            free to reach me anytime if you’d like to work together, share an
+            idea, or simply start a conversation.
+          </p>
+          <div className="mt-10 flex items-center justify-start">
+            <MdEmail size={25} color="#FFD803" className="mr-2" />
+            <p>chaydouble0@gmail.com</p>
+          </div>
+          <div className="flex items-center justify-start">
+            <IoMdPin size={25} color="#FFD803" className="mr-2" />
+            <p>334 Kampong Rou, Svay Rieng</p>
+          </div>
+          <div className="flex items-center justify-start">
+            <FaPhoneSquareAlt color="#FFD803" size={25} className="mr-2" />
+            <p>(855) 60446580</p>
+          </div>
+          <div className="flex items-center py-4 space-x-4">
+            <a
+              className="hover:scale-130 transition duration-75"
+              href="https://www.linkedin.com/in/meas-vanchay-9799b036a/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaLinkedin size={37} color="#FFD803" />
+            </a>
+            <a
+              className="hover:scale-130 transition duration-75"
+              href="https://www.facebook.com/vanchay.21/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaFacebook size={37} color="#FFD803" />
+            </a>
+            <a
+              className="hover:scale-130 transition duration-75"
+              href="https://github.com/Vanchay01"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaGithub size={37} color="#FFD803" />
+            </a>
+            <a
+              className="hover:scale-130 transition duration-75 "
+              href="https://t.me/vanchayyy"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaTelegram size={37} color="#FFD803" className="animate-wiggle" />
+            </a>
+          </div>
+        </div>
+        <div className="w-full md:w-1/2 md:flex justify-end">
+          <form
+            ref={form}
+            onSubmit={sendEmail}
+            className="w-full space-y-4 md:max-w-sm p-4 border-gray-900 dark:border-gray-100 rounded transition-colors duration-400 text-gray-700 dark:text-gray-300"
+          >
+            <input
+              type="hidden"
+              name="time"
+              value={new Date().toLocaleString()}
+            />
+            <input type="hidden" name="title" value="Website Contact" />
+            <label htmlFor="">Name</label>
+            <input
+              type="text"
+              name="name"
+              className="w-full p-3 border rounded border-gray-900 dark:border-gray-100 text-gray-700 dark:text-white transition-colors duration-400"
+              required
+            />
+            <label htmlFor="">Email</label>
+            <input
+              type="email"
+              name="user_email"
+              className="w-full p-3 border rounded border-gray-900 dark:border-gray-100 text-gray-700 dark:text-white transition-colors duration-400"
+              required
+            />
+            <label htmlFor="">Message</label>
+            <textarea
+              name="message"
+              className="w-full p-3 border rounded border-gray-900 dark:border-gray-100 text-gray-700 dark:text-white transition-colors duration-400"
+              rows="5"
+              required
+            ></textarea>
+            <button
+              type="submit"
+              className="w-full  box-button px-6 py-2 rounded cursor-pointer transition duration-400"
+            >
+              Send Message
+            </button>
+          </form>
+        </div>
+      </div>
+    </article>
   );
-}
+};
 
-export default Contact
+export default Contact;
