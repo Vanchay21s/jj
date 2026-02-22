de58d0060f4be6b7138805c04f6f49a0ae7f6645
vanchay21s <okpro21333@gmail.com>
2026-02-22 21:23:55 +0700

    Okok

--- src/components/Dashboad.jsx
+++ src/components/Dashboad.jsx
@@ -1,14 +1,18 @@
+import { useState } from "react"
 import { Link, Outlet} from "react-router-dom"
 
 const Dashboad = ({dark, setDark}) =>{
+
+    const [active, setActive] = useState(null)
+    console.log(active)
     return(
-        <article className="bg-dark border-b flex ">
-            <div className="w-96 h-screen bg-yellow-500 flex flex-col justify-center items-center drak-mode" >
-               <button onClick={() => setDark(!dark)} className="box-product m-4 items-center">Dark-Mode</button>
-                <Link className="box-product m-4 items-center" to="/profile">Profile</Link>
-                <Link className="box-product m-4 items-center" to="/skill">Skill</Link>
+        <article className="bg-dark border-b flex overflow-hidden">
+            <div className="w-96 h-screen bg-gray-800 flex flex-col dark-mode p-4" >
+               <button onClick={() => setDark(!dark)} className="btn-dashboard">Dark-Mode</button>
+                <Link className={"btn-dashboard " + (active === "profile" ? "bg-gray-700" : "")} onClick={()=>setActive("profile")}  to="/profile">Profile</Link>
+                <Link className={"btn-dashboard " + (active === "skill" ? "bg-gray-700" : "")} onClick={()=>setActive("skill")}  to="/skill">Skill</Link>
             </div>
-            <div className="w-full">
+            <div className="w-full h-screen bg-gray-800 p-4 overflow-hidden">
                 <Outlet />
             </div>
             
--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,7 +1,94 @@
+import { FaHome, FaSearch } from "react-icons/fa"
+
 const ProfilePage = () => {
+    const data = [
+        { id: 1, name: "Alice", age: 25 },
+        { id: 2, name: "Bob", age: 30 },
+        { id: 3, name: "Charlie", age: 22 },
+        { id: 4, name: "Diana", age: 28 },
+        { id: 5, name: "Ethan", age: 35 },
+        { id: 1, name: "Alice", age: 25 },
+        { id: 2, name: "Bob", age: 30 },
+        { id: 3, name: "Charlie", age: 22 },
+        { id: 4, name: "Diana", age: 28 },
+        { id: 5, name: "Ethan", age: 35 },
+        { id: 1, name: "Alice", age: 25 },
+        { id: 2, name: "Bob", age: 30 },
+        { id: 3, name: "Charlie", age: 22 },
+        { id: 4, name: "Diana", age: 28 },
+        { id: 5, name: "Ethan", age: 35 },
+        { id: 1, name: "Alice", age: 25 },
+        { id: 2, name: "Bob", age: 30 },
+        { id: 3, name: "Charlie", age: 22 },
+        { id: 4, name: "Diana", age: 28 },
+        { id: 5, name: "Ethan", age: 35 },
+        { id: 1, name: "Alice", age: 25 },
+        { id: 2, name: "Bob", age: 30 },
+        { id: 3, name: "Charlie", age: 22 },
+        { id: 4, name: "Diana", age: 28 },
+        { id: 5, name: "Ethan", age: 35 },
+        { id: 1, name: "Alice", age: 25 },
+        { id: 2, name: "Bob", age: 30 },
+        { id: 3, name: "Charlie", age: 22 },
+        { id: 4, name: "Diana", age: 28 },
+        { id: 5, name: "Ethan", age: 35 },
+        { id: 1, name: "Alice", age: 25 },
+        { id: 2, name: "Bob", age: 30 },
+        { id: 3, name: "Charlie", age: 22 },
+        { id: 4, name: "Diana", age: 28 },
+        { id: 5, name: "Ethan", age: 35 },
+        { id: 1, name: "Alice", age: 25 },
+        { id: 2, name: "Bob", age: 30 },
+        { id: 3, name: "Charlie", age: 22 },
+        { id: 4, name: "Diana", age: 28 },
+        { id: 5, name: "Ethan", age: 35 },
+        { id: 1, name: "Alice", age: 25 },
+        { id: 2, name: "Bob", age: 30 },
+        { id: 3, name: "Charlie", age: 22 },
+        { id: 4, name: "Diana", age: 28 },
+        { id: 5, name: "Ethan", age: 35 },
+        { id: 1, name: "Alice", age: 25 },
+        { id: 2, name: "Bob", age: 30 },
+        { id: 3, name: "Charlie", age: 22 },
+        { id: 4, name: "Diana", age: 28 },
+        { id: 5, name: "Ethan", age: 35 },
+        { id: 1, name: "Alice", age: 25 },
+        { id: 2, name: "Bob", age: 30 },
+        { id: 3, name: "Charlie", age: 22 },
+        { id: 4, name: "Diana", age: 28 },
+        { id: 5, name: "Ethan", age: 35 },
+        { id: 1, name: "Alice", age: 25 },
+        { id: 2, name: "Bob", age: 30 },
+        { id: 3, name: "Charlie", age: 22 },
+        { id: 4, name: "Diana", age: 28 },
+        { id: 5, name: "Ethan", age: 35 },
+    ];
     return (
-        <article>
-            <h1>ProfilePage</h1>
+        <article className="w-full h-full flex flex-col">
+            <div className={`flex justify-between items-center gap-4 pb-2 border-b border-gray-700`}>
+                <FaHome className="text-2xl text-gray-400" />
+                <div className="relative bg-gray-700 w-96 rounded-full">
+                    <FaSearch className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400" />
+                    <input 
+                        type="text" 
+                        placeholder="Skill, work, etc" 
+                        className="focus:outline-0 text-base py-3 pl-10 p-4 w-full"
+                    />
+                </div>
+            </div>
+            <div className="w-full flex-1 flex mt-2 gap-4 overflow-hidden scrtollbar-hide">
+                <div className="w-4/6 h-full bg-gray-600 overflow-y-auto ">
+                    {
+                        data.map((d, i) => (
+                            <div key={d.id} className={`${i % 2 === 0 ? 'bg-gray-500':'bg-gray-600'} p-2 border-b `}>
+                                <div className="font-bold">{d.name}</div>
+                                <div>Age: {d.age}</div>
+                            </div>
+                        ))
+                    }
+                </div>
+                <div className="w-2/6 bg-gray-500">Insert</div>
+            </div>
         </article>
     )
 }
