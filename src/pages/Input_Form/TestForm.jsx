41b10efed49bb430d96799bcf97e4405e7bc3982
Vanchay21 <okpro21333@gmail.com>
2026-02-28 21:45:03 +0700

    Addd Form Skill

--- src/pages/Input_Form/TestForm.jsx
+++ src/pages/Input_Form/TestForm.jsx
@@ -1,5 +1,5 @@
 import { article } from "motion/react-client";
-import { useState } from "react";
+import { use, useId, useState } from "react";
 
 const TestForm = () => {
   const [username, setUsername] = useState("");
@@ -12,7 +12,11 @@ const TestForm = () => {
   const [passwork, setPasswork] = useState("");
   const [image, setImage] = useState(null);
   const [preview, setPreview] = useState(null);
+  // Skill
+  const [nameSkill, setNameSkill] = useState("")
+  const [rating, setRating] = useState("")
 
+  // HandleImageChange
   const handleImageChange = (e) => {
     const file = e.target.files[0];
     setImage(file);
@@ -20,6 +24,18 @@ const TestForm = () => {
       setPreview(URL.createObjectURL(file));
     }
   };
+
+  const [imgSkill, setImgSkill] = useState(null)
+  const [skillView, setSkillView] = useState(null)
+  const handleImgSkill = (e) =>{
+    const file = e.target.files[0]
+    setImgSkill(file)
+    if (file){
+      setSkillView(URL.createObjectURL(file))
+    }
+  }
+
+  // Handle Profile
   const handleSubmitProfile = (e) => {
     e.preventDefault();
     alert(
@@ -34,7 +50,15 @@ const TestForm = () => {
     setDate("");
     setPasswork("");
   };
+  // Handle Skill
+  const handleSubmitSkill = (e) =>{
+    e.preventDefault();
+    alert("ok")
+  }
 
+
+  // Console...
+  console.log(ok)
   console.log(username);
   return (
     <article className="bg-gray-300 ">
@@ -79,11 +103,13 @@ const TestForm = () => {
           onChange={(e) => setAddress(e.target.value)}
           className={`form-input`}
         />
-        <input
+        <textarea
           type="text"
           value={about}
           placeholder="About"
           onChange={(e) => setAbout(e.target.value)}
+          cols={20}
+          rows={5}
           className={`form-input`}
         />
         <input
@@ -109,8 +135,34 @@ const TestForm = () => {
         <button className={`form-input`}>OK</button>
       </form>
       {/* Skill */}
-       
-
+       <form onSubmit={handleSubmitSkill}
+          className="w-full max-w-7xl m-auto py-3 flex flex-col gap-2"
+       >
+          <input type="text"
+            value={nameSkill}
+            placeholder="name"
+            onChange={(e) => setNameSkill(e.target.value)}
+            className={`form-input `}
+          />
+          <input type="text"
+            value={rating}
+            placeholder="rating"
+            onChange={(e) => setRating(e.target.value)}
+            className={`form-input`}
+          />
+          <input type="file"
+            accept="image/*"
+            onChange={handleImgSkill}
+          />
+          {skillView && (
+            <img
+              src={skillView}
+              alt="Preview"
+              className="w-24 h-24 object-cover rounded-lg"
+            />
+          )}
+          <button className="form">Add</button>
+       </form>
       {/* Work */}
     </article>
   );
