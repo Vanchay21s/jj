6bc2f2ab4329ee0a1bf106840c60e5fdcfb35ddc
Vanchay <102397819+Vanchay01@users.noreply.github.com>
2026-03-03 22:34:46 +0700

    asdsa

--- src/components/Dashboad.jsx
+++ src/components/Dashboad.jsx
@@ -6,13 +6,13 @@ const Dashboad = ({dark, setDark}) =>{
     const [active, setActive] = useState(null)
     return(
         <article className="bg-dark border-b flex overflow-hidden">
-            <div className="w-96 h-screen bg-gray-800 flex flex-col dark-mode p-4" >
+            <div className="w-96 bg-gray-800 flex flex-col dark-mode p-4" >
                <button onClick={() => setDark(!dark)} className="btn-dashboard">Dark-Mode</button>
                 <Link className={"btn-dashboard " + (active === "profile" ? "bg-gray-700" : "")} onClick={()=>setActive("profile")}  to="/profile">Profile</Link>
                 <Link className={"btn-dashboard " + (active === "skill" ? "bg-gray-700" : "")} onClick={()=>setActive("skill")}  to="/skill">Skill</Link>
                 <Link className={"btn-dashboard " + (active === "feature" ? "bg-gray-700" : "")} onClick={()=>setActive("feature")}  to="/feature">Feature</Link>
             </div>
-            <div className="w-full h-screen bg-gray-800 p-4 overflow-hidden">
+            <div className="w-full bg-gray-800 p-4 overflow-hidden">
                 <Outlet />
             </div>
         </article>
--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -18,7 +18,7 @@ const initState = {
 };
 const ProfilePage = () => {
   const [formData, setFormData] = useState(initState);
-  const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(false);
 
   const handleChangeInput = (e) => {
     const { name, value, type, files } = e.target;
@@ -38,7 +38,7 @@ const ProfilePage = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(formData)
+    console.log(formData);
     alert(`
         username: ${formData.username}
         name: ${formData.name}
@@ -47,15 +47,18 @@ const ProfilePage = () => {
         phone: ${formData.phone}
         date: ${formData.date}
         address: ${formData.address}
-    `)
+    `);
   };
 
   return (
-    <article className="w-full h-full flex flex-col">
+    <article className="w-full flex flex-col">
       <h1 className="text-2xl w-full pb-4 border-b border-gray-400">Profile</h1>
-      <button 
-      onClick={() => setOpen(true)}
-      className="absolute right-4 mt-2 text-sm cursor-pointer  flex justify-center items-center border-b">Change password</button>
+      <button
+        onClick={() => setOpen(true)}
+        className="absolute right-4 mt-2 text-sm cursor-pointer flex justify-center items-center border-b"
+      >
+        Change password
+      </button>
       <form
         onSubmit={handleSubmit}
         className="w-full h-full mt-2 flex flex-col justify-between"
@@ -83,7 +86,7 @@ const ProfilePage = () => {
               type="file"
               accept="image/*"
               onChange={handleChangeInput}
-              className="absolute top-30 border rounded-lg bg-gray-800 hidden "
+              className="absolute top-30 border rounded-lg    hidden "
             />
             <label
               htmlFor="editInput"
@@ -174,16 +177,56 @@ const ProfilePage = () => {
           <FaSave /> Save
         </button>
       </form>
-      {
-        open && <div className="fixed inset-0 right-0 w-90 flex items-right justify-right bg-black bg-opacity-50" >
-          <h1 className="text-sm w-full pb-4 border-b border-gray-400">Change password</h1>
-          <button onClick={() => setOpen(false)} className="absolute top-0 right-4 text-sm border-b cursor-pointer">close</button>
-          <form action="">
-
-            sdsd  
-          </form>
+      {open && (
+        <div className="fixed inset-0 flex justify-end bg-black/50 bg-opacity-100">
+          <div className="w-90 p-2 bg-gray-800 start-0">
+            <div className="flex items-center justify-between border-b mt-1 pb-4 border-gray-400">
+              <h1 className="text-sm w-full mt-4">Change password</h1>
+              <button
+                onClick={() => setOpen(false)}
+                className="text-sm mt-4 border-b cursor-pointer"
+              >
+                close
+              </button>
+            </div>
+            <form className="flex flex-col">
+              <h1 className="text-lg mt-2">Old password</h1>
+              <input
+                type="text"
+                name="old-password"
+                placeholder="Old password"
+                className={` border focus:border-blue-600 border-gray-400 focus:outline-none text-lg py-1 px-4 rounded-lg mt-2 `}
+              />
+              <h1 className="text-lg mt-2">New password</h1>
+              <input
+                type="text"
+                name="new-password"
+                placeholder="New password"
+                className={` border focus:border-blue-600 border-gray-400 focus:outline-none text-lg py-1 px-4 rounded-lg mt-2 `}
+              />
+              <h1 className="text-lg mt-2">Confirm password</h1>
+              <input
+                type="text"
+                name="confirm-password"
+                placeholder="Confirm password"
+                className={` border focus:border-blue-600 border-gray-400 focus:outline-none text-lg py-1 px-4 rounded-lg mt-2 `}
+              />
+              <div className="absolute bottom-4 flex items-center justify-center mt-2 gap-2">
+                <button
+                  className={`w-1/2 text-lg mt-2 py-1 px-4 border rounded-lg flex justify-center items-center gap-2 cursor-pointer`}
+                >
+                  Cancel
+                </button>
+                <button
+                  className={`w-1/2 text-lg mt-2 py-1 px-4 border rounded-lg flex justify-center items-center gap-2 cursor-pointer`}
+                >
+                  Change
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
-      }
+      )}
     </article>
   );
 };
